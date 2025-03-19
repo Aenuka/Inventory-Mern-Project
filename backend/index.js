7910c17f5afe05fdac8a77a50484ad830621130c
Aenuka Buddhakorala <aenuka.elakollek@gmail.com>
2025-03-19 15:56:34 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 // MongoBD is Connection
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://admin:xOwYr6lEwpR1OzQW@cluster0.g5rlw.mongodb.net/?appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,18 +31,38 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
     //create a collections of document
     const inventoryCollections = client.db("ahmsInventory").collection("Medications");
 
     // insert medicine to the db : post method
 
-    app.post("/upload-inven", async(req, res) => {
+    app.post("/update-inventory", async(req, res) => {
         const data = req.body;
         const result = await inventoryCollections.insertOne(data);
         res.send(result)
     })
 
 
+    // get all the data
+
+    app.get("/all-items", async(req, res) => {
+      const items = inventoryCollections.find()
+      const result = await items.toArray();
+      res.send(result);
+
+    })
+
+    // update a item data : patch or post or update method
+
+    app.patch("/inventory/:id", async(req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const updateItemData = req.body;
+
+      const filter = {_id: new ObjectId(id)}
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
